9899dfd2a82241f69c758d68b667341df1a428c9
Eric <eric@opencapital>
2016-11-04 22:05:01 -0700

    #2 fix up specs generated by scaffold

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -19,141 +19,139 @@ require 'rails_helper'
 # that an instance is receiving a specific message.
 
 RSpec.describe WordsController, type: :controller do
-
   # This should return the minimal set of attributes required to create a valid
   # Word. As you add validations to Word, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    attributes_for(:word)
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    attributes_for(:word, word: nil)
+  end
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # WordsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "assigns all words as @words" do
+  describe 'GET #index' do
+    it 'assigns all words as @words' do
       word = Word.create! valid_attributes
       get :index, params: {}, session: valid_session
       expect(assigns(:words)).to eq([word])
     end
   end
 
-  describe "GET #show" do
-    it "assigns the requested word as @word" do
+  describe 'GET #show' do
+    it 'assigns the requested word as @word' do
       word = Word.create! valid_attributes
-      get :show, params: {id: word.to_param}, session: valid_session
+      get :show, params: { id: word.to_param }, session: valid_session
       expect(assigns(:word)).to eq(word)
     end
   end
 
-  describe "GET #new" do
-    it "assigns a new word as @word" do
+  describe 'GET #new' do
+    it 'assigns a new word as @word' do
       get :new, params: {}, session: valid_session
       expect(assigns(:word)).to be_a_new(Word)
     end
   end
 
-  describe "GET #edit" do
-    it "assigns the requested word as @word" do
+  describe 'GET #edit' do
+    it 'assigns the requested word as @word' do
       word = Word.create! valid_attributes
-      get :edit, params: {id: word.to_param}, session: valid_session
+      get :edit, params: { id: word.to_param }, session: valid_session
       expect(assigns(:word)).to eq(word)
     end
   end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Word" do
-        expect {
-          post :create, params: {word: valid_attributes}, session: valid_session
-        }.to change(Word, :count).by(1)
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new Word' do
+        expect do
+          post :create, params: { word: valid_attributes }, session: valid_session
+        end.to change(Word, :count).by(1)
       end
 
-      it "assigns a newly created word as @word" do
-        post :create, params: {word: valid_attributes}, session: valid_session
+      it 'assigns a newly created word as @word' do
+        post :create, params: { word: valid_attributes }, session: valid_session
         expect(assigns(:word)).to be_a(Word)
         expect(assigns(:word)).to be_persisted
       end
 
-      it "redirects to the created word" do
-        post :create, params: {word: valid_attributes}, session: valid_session
+      it 'redirects to the created word' do
+        post :create, params: { word: valid_attributes }, session: valid_session
         expect(response).to redirect_to(Word.last)
       end
     end
 
-    context "with invalid params" do
-      it "assigns a newly created but unsaved word as @word" do
-        post :create, params: {word: invalid_attributes}, session: valid_session
+    context 'with invalid params' do
+      it 'assigns a newly created but unsaved word as @word' do
+        post :create, params: { word: invalid_attributes }, session: valid_session
         expect(assigns(:word)).to be_a_new(Word)
       end
 
       it "re-renders the 'new' template" do
-        post :create, params: {word: invalid_attributes}, session: valid_session
-        expect(response).to render_template("new")
+        post :create, params: { word: invalid_attributes }, session: valid_session
+        expect(response).to render_template('new')
       end
     end
   end
 
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested word" do
-        word = Word.create! valid_attributes
-        put :update, params: {id: word.to_param, word: new_attributes}, session: valid_session
-        word.reload
-        skip("Add assertions for updated state")
+  describe 'PUT #update' do
+    context 'with valid params' do
+      let(:new_attributes) do
+        { word: 'new word' }
       end
 
-      it "assigns the requested word as @word" do
+      it 'updates the requested word' do
         word = Word.create! valid_attributes
-        put :update, params: {id: word.to_param, word: valid_attributes}, session: valid_session
+        put :update, params: { id: word.to_param, word: new_attributes }, session: valid_session
+        word.reload
+        expect(word.word).to eq('new word')
+      end
+
+      it 'assigns the requested word as @word' do
+        word = Word.create! valid_attributes
+        put :update, params: { id: word.to_param, word: valid_attributes }, session: valid_session
         expect(assigns(:word)).to eq(word)
       end
 
-      it "redirects to the word" do
+      it 'redirects to the word' do
         word = Word.create! valid_attributes
-        put :update, params: {id: word.to_param, word: valid_attributes}, session: valid_session
+        put :update, params: { id: word.to_param, word: valid_attributes }, session: valid_session
         expect(response).to redirect_to(word)
       end
     end
 
-    context "with invalid params" do
-      it "assigns the word as @word" do
+    context 'with invalid params' do
+      it 'assigns the word as @word' do
         word = Word.create! valid_attributes
-        put :update, params: {id: word.to_param, word: invalid_attributes}, session: valid_session
+        put :update, params: { id: word.to_param, word: invalid_attributes }, session: valid_session
         expect(assigns(:word)).to eq(word)
       end
 
       it "re-renders the 'edit' template" do
         word = Word.create! valid_attributes
-        put :update, params: {id: word.to_param, word: invalid_attributes}, session: valid_session
-        expect(response).to render_template("edit")
+        put :update, params: { id: word.to_param, word: invalid_attributes }, session: valid_session
+        expect(response).to render_template('edit')
       end
     end
   end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested word" do
+  describe 'DELETE #destroy' do
+    it 'destroys the requested word' do
       word = Word.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: word.to_param}, session: valid_session
-      }.to change(Word, :count).by(-1)
+      expect do
+        delete :destroy, params: { id: word.to_param }, session: valid_session
+      end.to change(Word, :count).by(-1)
     end
 
-    it "redirects to the words list" do
+    it 'redirects to the words list' do
       word = Word.create! valid_attributes
-      delete :destroy, params: {id: word.to_param}, session: valid_session
+      delete :destroy, params: { id: word.to_param }, session: valid_session
       expect(response).to redirect_to(words_url)
     end
   end
-
 end
