9bd8323f4b99e9e1411be36dc373e8faa3f122e7
Eric <eric@opencapital>
2016-11-04 21:55:31 -0700

    #2 refactor test to use factories

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -4,14 +4,14 @@ require 'faker'
 RSpec.describe Word, type: :model do
   describe 'validations' do
     it 'allows valid values for word' do
-      word = Word.new(word: Faker::Lorem.word())
+      word = build(:word, word: Faker::Lorem.word())
       expect(word.valid?).to eq(true), "test failed with #{word.word}"
     end
 
     it 'disallows invalid values for word' do
-      word = Word.new(word: nil)
+      word = build(:word, word: nil)
       expect(word.valid?).to eq(false)
-      word = Word.new(word: Faker::Lorem.characters(256))
+      word = build(:word, word: Faker::Lorem.characters(256))
       expect(word.valid?).to eq(false), "test failed with word #{word.word}"
     end
   end
